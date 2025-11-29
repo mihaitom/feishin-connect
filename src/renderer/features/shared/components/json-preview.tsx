93ba24285347c6d17e1f0a4b225d8da948bf6c13
jeffvli <jeffvictorli@gmail.com>
2025-11-29 13:30:23 -0800

    update JsonPreview to use Code component

--- src/renderer/features/shared/components/json-preview.tsx
+++ src/renderer/features/shared/components/json-preview.tsx
@@ -1,13 +1,15 @@
 import styles from './json-preview.module.css';
 
+import { Code } from '/@/shared/components/code/code';
+
 interface JsonPreviewProps {
     value: Record<string, any> | string;
 }
 
 export const JsonPreview = ({ value }: JsonPreviewProps) => {
     return (
-        <pre className={styles.preview} style={{ userSelect: 'all' }}>
+        <Code block className={styles.preview} lang="json" p="md">
             {JSON.stringify(value, null, 4)}
-        </pre>
+        </Code>
     );
 };
