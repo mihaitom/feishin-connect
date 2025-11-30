7e3b42af32e0e58d63bc6ad91f3d728b98c68c82
jeffvli <jeffvictorli@gmail.com>
2025-11-29 20:18:55 -0800

    add edit playlist to context menu

--- src/renderer/features/context-menu/menus/playlist-context-menu.tsx
+++ src/renderer/features/context-menu/menus/playlist-context-menu.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 
 import { AddToPlaylistAction } from '/@/renderer/features/context-menu/actions/add-to-playlist-action';
 import { DeletePlaylistAction } from '/@/renderer/features/context-menu/actions/delete-playlist-action';
+import { EditPlaylistAction } from '/@/renderer/features/context-menu/actions/edit-playlist-action';
 import { GetInfoAction } from '/@/renderer/features/context-menu/actions/get-info-action';
 import { PlayAction } from '/@/renderer/features/context-menu/actions/play-action';
 import { ContextMenu } from '/@/shared/components/context-menu/context-menu';
@@ -29,6 +30,7 @@ export const PlaylistContextMenu = ({ items, type }: PlaylistContextMenuProps) =
             <ContextMenu.Divider />
             <GetInfoAction disabled={items.length === 0} item={items[0]} />
             <ContextMenu.Divider />
+            <EditPlaylistAction items={items} />
             <DeletePlaylistAction items={items} />
         </ContextMenu.Content>
     );
